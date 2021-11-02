c997d79c050235f0e76ac56b347466429565167b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-11-02 22:15:53 +0900

    feat: first commit

---
+++ android/app/src/main/kotlin/com/kosukesaigusa/flutter_firebase_emulator_suite/firebase_emulator_suite_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kosukesaigusa.flutter_firebase_emulator_suite.firebase_emulator_suite_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
